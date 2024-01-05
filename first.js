2dc672624dd44fdd995b4a004a5e53a966118edf
John Michael <111406110+yahole@users.noreply.github.com>
2024-01-04 21:16:50 -0800

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+// var myDiv = document.querySelector('div');
+// myDiv.addEventListener('click', handleClick, false);
+
+// function handleClick(e){
+//     if (e.target !== e.currentTarget) {
+//         var clickItem = e.target.matches('drum' );
+//         alert('it clicked me');
+//     }    
+//     e.stopPropagation();
+// }
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,27 @@
+// document.querySelector('button').addEventListener("click", handleClick);
+
+
+
+var clickableButtons = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i<clickableButtons; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener("click", function (){
+   
+
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+
+    });
+}
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
